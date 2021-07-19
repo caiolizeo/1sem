1ec5f4fa54f3585f0f528cf8561601cfa72de104
caiolizeo <rm87809@fiap.com.br>
2021-07-19 19:51:37 -0300

    adc verificações

--- CTP/CP3/cp3.py
+++ CTP/CP3/cp3.py
@@ -5,7 +5,7 @@ qtdInscrito = []
 
 cadastrado = False
 
-vagas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+vagas = [10, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 '''
 Posicoes:
 0 Historia - 2a e 3a série
@@ -63,7 +63,6 @@ while(menu != 0):
                         print("Aluno  não  cadastrado. Favor procurar a coordenação do Fundamental I")
                         rm = int(input("Digite o RM do aluno \n---> "))
                 serieAtual = serie[matricula.index(rm)]
-                print(serieAtual)
 
                 hist = "1 - Criar e contar histórias(Vagas disponiveis: {})".format(10-vagas[0])
                 sinais = "2 - A língua de sinais (Vagas disponiveis: {})".format(10-vagas[2])
@@ -71,42 +70,62 @@ while(menu != 0):
                 emoji = "4 - Criando e recriando com emojis (Vagas disponiveis: {})".format(10-vagas[9])
 
                 if(vagas[0] == 10):
-                        hist = "1 - INDISPONIVEL"
+                        hist = "*** INDISPONIVEL ***"
+                if(vagas[1] == 10):
+                        hist = "*** INDISPONIVEL ***"
                 if(vagas[2] == 10):
-                        sinais = "2 - INDISPONIVEL"
+                        sinais = "*** INDISPONIVEL ***"
                 if(vagas[7] == 10):
-                        mundo = "3 - INDISPONIVEL"
+                        mundo = "*** INDISPONIVEL ***"
                 if(vagas[9] == 10):
-                        emoji = "4 - INDISPONIVEL"
+                        emoji = "*** INDISPONIVEL ***"
                 
                 if(serieAtual == 2):
                         print("Escolha um evento \n{} \n{} \n{} \n{}".format(hist, sinais, mundo, emoji))
                         seleciona = int(input("---> "))
 
-                        if(seleciona == 1 and rm not in arrHistoria):
-                                valor = vagas[0] + 1
-                                vagas[0] = valor
-                                arrHistoria.append(rm)
-                                print("ALUNO CADASTRADO")
-                        elif(seleciona == 2 and rm not in arrLingSinais):
-                                valor = vagas[2] + 1
-                                vagas[2] = valor
-                                arrLingSinais.append(rm)
-                                print("ALUNO CADASTRADO")
-                        elif(seleciona == 3 and rm not in arrMundoImag):
-                                valor = vagas[7] + 1
-                                vagas[7] = valor
-                                arrLingSinais.append(rm)
-                                print("ALUNO CADASTRADO")
-                        elif(seleciona == 4 and rm not in arrCriandoEmoji):
-                                valor = vagas[9] + 1
-                                vagas[9] = valor
-                                arrCriandoEmoji.append(rm)
-                                print("ALUNO CADASTRADO")
+                        cadastrou = False
+                        if(qtdInscrito[matricula.index(rm)] < 3):
+                                print(qtdInscrito[matricula.index(rm)])
+                                if(seleciona == 1 and rm not in arrHistoria and vagas[0] < 10):
+                                        valor = vagas[0] + 1
+                                        vagas[0] = valor
+                                        arrHistoria.append(rm)
+                                        print("ALUNO CADASTRADO")
+                                        cadastrou = True
+                                elif(seleciona == 2 and rm not in arrLingSinais):
+                                        valor = vagas[2] + 1
+                                        vagas[2] = valor
+                                        arrLingSinais.append(rm)
+                                        print("ALUNO CADASTRADO")
+                                        cadastrou = True
+                                elif(seleciona == 3 and rm not in arrMundoImag):
+                                        valor = vagas[7] + 1
+                                        vagas[7] = valor
+                                        arrMundoImag.append(rm)
+                                        print("ALUNO CADASTRADO")
+                                        cadastrou = True
+                                elif(seleciona == 4 and rm not in arrCriandoEmoji):
+                                        valor = vagas[9] + 1
+                                        vagas[9] = valor
+                                        arrCriandoEmoji.append(rm)
+                                        print("ALUNO CADASTRADO")
+                                        cadastrou = True
+                                elif(rm in arrHistoria or rm in arrLingSinais or rm in arrMundoImag or rm in arrMundoImag):
+                                        print("ALUNO JA FOI CADASTRADO")
+                                if(cadastrou):
+                                        valor = qtdInscrito[matricula.index(rm)] + 1
+                                        qtdInscrito[matricula.index(rm)] = valor
+                        else:
+                                print("APENAS TRÊS INSCRIÇÕES POR ALUNO")
+                
+                if(serieAtual == 3):
+                         print("Escolha um evento \n{} \n{} \n{} \n{}".format(hist, sinais, mundo, emoji))
+                         seleciona = int(input("---> "))
 
-                        elif(rm in arrHistoria):
-                                print("ALUNO JA FOI CADASTRADO")
 
+        elif(menu == 4):
+                break
 
 
 
@@ -115,3 +134,4 @@ while(menu != 0):
 print(nomeAluno)
 print(matricula)
 print(serie)
+print(qtdInscrito)
--- CTP/CP3/teste.py
+++ CTP/CP3/teste.py
@@ -1,6 +1,51 @@
-teste = [2,3,5,2,4,5]
-testando = [325,355,458,5]
+nomeAluno = ["João", "Maria", "Marcos"]
+matricula = [111, 222, 333]
+serie = [2, 2, 2]
+qtdInscrito = [0, 0, 0]
 
-soma = teste[0] + 1
+cadastrado = False
 
-print(soma)
+vagas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+'''
+Posicoes:
+0 Historia - 2a e 3a série
+1 Teatro - 3a e 4a série
+2 Lingua de Sinais - Todos
+3 Expressao Artistica - 4a e 5a série
+4 Soletrando - 5a série
+5 Leitura Dinamica - 4a série
+6 O Corpo Fala - 3a série
+7 O Mundo da Imaginacao - 2a série
+8 Leitura Dinamica - 5a série
+9 Criando Emojis - 2a série
+'''
+arrHistoria = []       #2a e 3a série
+arrTeatro = []         #3a e 4a série
+arrLingSinais = []     #Todos
+arrExpArtist = []      #4a e 5a série  
+arrSoletrando = []     #5a série
+arrLeitDinamica4a = [] #4a série     
+arrCorpoFala = []      #3a série  
+arrMundoImag = []      #2a série
+arrLeitDinamica5a = [] #5a série
+arrCriandoEmoji = []   #2a série
+
+cadastrou = True
+
+
+
+rm = int(input("Digite o rm: "))
+while(rm != 0):
+    if(qtdInscrito[matricula.index(rm)] < 3):
+        valor = qtdInscrito[matricula.index(rm)] + 1
+        qtdInscrito[matricula.index(rm)] = valor
+    else:
+        print("LIMITE DE 3")
+    rm = int(input("Digite o rm: "))
+
+
+
+print(nomeAluno)
+print(matricula)
+print(serie)
+print(qtdInscrito)
