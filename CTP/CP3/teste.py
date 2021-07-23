55ed3caf54e9a6964454766076da340f9a4c7c7f
caiolizeo <rm87809@fiap.com.br>
2021-07-23 19:52:17 -0300

    testando a listagem

--- CTP/CP3/teste.py
+++ CTP/CP3/teste.py
@@ -1,15 +1,15 @@
 nomeAluno = ["João", "Maria", "Marcos", "Zico", "Antonio"]
-numMatricula = [111, 222, 333, 559, 668]
-serie = [2, 2, 2]
-qtdInscrito = [0, 0, 0]
+numMatricula = [111, 222, 333, 444, 555]
+serie = [2, 3, 4, 2, 5]
+qtdInscrito = [0, 0, 0, 0, 0]
 
 cadastrado = False
 
 vagas = [0]*10
 
-arrHist = [111, 559, 668]            #2a e 3a série
-arrTeatro = [114, 354, 475]          #3a e 4a série
-arrSinais = [123, 559, 668] 
+arrHist = [111, 222, 333]           
+arrTeatro = [111, 333, 555]          
+arrSinais = [111, 444, 555] 
 
 oficinas = (arrHist, arrTeatro, arrSinais)
 
@@ -31,16 +31,27 @@ def listaAluno(numMatricula, nome, serieAluno):
             aluno = (numMatricula, nome, serieAluno, inscricoes[0])
 
     if(len(aluno) == 6):
-            x = "RM: {} - {} - {}ª. série \nOficinas: \n{} \n{} \n{}".format(aluno[0], aluno[1], aluno[2], aluno[3], aluno[4], aluno[5])
+            x = "RM: {} - {} - {}ª. série \nOficinas: \n{} \n{} \n{}\n".format(aluno[0], aluno[1], aluno[2], aluno[3], aluno[4], aluno[5])
     elif(len(aluno) == 5):
-            x = "RM: {} - {} - {}ª. série \nOficinas: \n{} \n{}".format(aluno[0], aluno[1], aluno[2], aluno[3], aluno[4])
+            x = "RM: {} - {} - {}ª. série \nOficinas: \n{} \n{}\n".format(aluno[0], aluno[1], aluno[2], aluno[3], aluno[4])
     elif(len(aluno) == 4):
-            x = "RM: {} - {} - {}ª. série \nOficinas: \n{}".format(aluno[0], aluno[1], aluno[2], aluno[3])
+            x = "RM: {} - {} - {}ª. série \nOficinas: \n{}\n".format(aluno[0], aluno[1], aluno[2], aluno[3])
 
     return x
 
 aluno = listaAluno(numMatricula[0], nomeAluno[0], serie[0])
-print(aluno)
+#print(aluno)
+
+def ordenaLista(nome):
+    qtdAlunos = len(nome)
+    cont = 0
+    while(cont < qtdAlunos):
+        list = listaAluno(numMatricula[cont], nomeAluno[cont], serie[cont])
+        print(list)
+        cont+=1
+
+
+ordenaLista(nomeAluno)
 
 '''
 aluno = (nomeAluno[0], matricula[0], serie[0], qtdInscrito[0])
